26b9b558ccd9f7ce1d0b4e0c47fbe07eec4b9545
rworron <rworron94@gmail.com>
2026-03-01 15:43:05 -0500

    implemented a timer for GoogleReviews.tsx so that LCP is lowered

--- app/components/GoogleReviews.tsx
+++ app/components/GoogleReviews.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import Script from "next/script";
+import { useEffect, useState } from "react";
 
 export default function GoogleReviews() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoaded(true), 1000); // load after 1s
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!loaded) return null;
+
   return (
-    <>
+    <div
+      className="elfsight-app-7611b343-38fb-46cc-967e-4a442156b452"
+      data-elfsight-app-lazy
+    >
       <Script src="https://elfsightcdn.com/platform.js" strategy="lazyOnload" />
-      <div
-        className="elfsight-app-7611b343-38fb-46cc-967e-4a442156b452"
-        data-elfsight-app-lazy
-      ></div>
-    </>
+    </div>
   );
 }
